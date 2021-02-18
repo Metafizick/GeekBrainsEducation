b0e534cf660726f16beb8e3707bdb81bf405160f
AndreyB <metafizick@ya.ru>
2021-02-18 03:44:17 +0300

    Lesson-1 complete

---
+++ Lesson-1/Lesson-1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите своё имя:");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Приветствую, {name}, Сегодня { DateTime.Now }");
+        }
+    }
+}
