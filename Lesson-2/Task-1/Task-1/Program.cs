5bfedd8b3d2c8e038b1bc4686230fb9c088b7ff3
AndreyB <metafizick@ya.ru>
2021-02-23 02:11:37 +0300

    Ready task 1,2,5

--- Lesson-2/Task-1/Task-1/Program.cs
+++ Lesson-2/Task-1/Task-1/Program.cs
@@ -8,11 +8,25 @@ namespace Task_1
 {
     class Program
     {
+        enum months { 
+            January=1, 
+            Februry = 2, 
+            March = 3,
+            April = 4, 
+            May = 5, 
+            June = 6,
+            July = 7,
+            August = 8, 
+            September = 9, 
+            October = 10, 
+            November = 11, 
+            December = 12 }
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to ThermoMetrium!");
-            bool minBool, maxBool;
+            bool minBool, maxBool,monBool;
             double min, max;
+            int mon;
             do
              {
                  Console.Write("Please enter a minimal temperature in Celsius: ");
@@ -35,6 +49,76 @@ namespace Task_1
             } while (maxBool == false);
             double average = (max + min) / 2;
             Console.WriteLine($"Average temperature of the day = {average}");
+            do
+            {
+                Console.Write("Please enter a number of current month: ");
+                string monStr = Console.ReadLine();
+                monBool = Int32.TryParse(monStr, out mon);
+                if (monBool == false)
+                {
+                    Console.WriteLine("Incorrected value of number. Try again");
+                }
+            } while (monBool == false);
+            Console.WriteLine("month is: {0}", Enum.GetName(typeof(months), mon));
+            switch (mon)
+            {
+                case 12:
+                case 1:
+                case 2:
+                    if (average>=0)
+                    {
+                        Console.WriteLine("Winter in Europa");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Winter in Russia");
+                    }
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    if (average<=0)
+                    {
+                        Console.WriteLine("Abnormally cold spring");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Normal spring, season of love:)");
+                    }
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    if (average>30)
+                    {
+                        Console.WriteLine("Hot summer");
+                    }
+                    else if (average<10)
+                    {
+                        Console.WriteLine("Abnormally cold summer");
+                    }
+                    else
+                    {
+                        Console.WriteLine("summer in Russia");
+                    }
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    if (average>15)
+                    {
+                        Console.WriteLine("Very good autumn!");
+                    }
+                    else if (average<0)
+                    {
+                        Console.WriteLine("Abnormally cold autumn!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Autumn in Russia");
+                    }
+                    break;
+            }
             Console.ReadKey();
         }
     }
