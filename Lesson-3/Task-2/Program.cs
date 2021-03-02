092264b327585ca8fa8d2204eb65417f7938f85c
metafizick@ya.ru <metafizick@ya.ru>
2021-03-02 14:50:09 +0300

    Task-2

---
+++ Lesson-3/Task-2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("PhoneBook");
+            String[,] phone = new String[5, 2];
+            for (int i = 0; i<phone.GetLength(0); i++)
+            {
+                Console.Write($"Enter {i} phone/email adress");
+                phone[i, 0] = Console.ReadLine();
+            }
+            for (int i = 0; i < phone.GetLength(0); i++)
+            {
+                Console.Write($"Enter {i} name");
+                phone[i, 1] = Console.ReadLine();
+            }
+            Console.WriteLine({(0,0)} {(1,15)}"Phone/email", "name");
+            for (int i=0; i<phone.GetLength(0); i++)
+            {
+                for (int j=0; j<phone.GetLength(1); j++)
+                {
+                    Console.Write(phone[i, j], "       ");
+                }
+                Console.WriteLine("\t");
+            }
+            Console.ReadKey();
+        }
+    }
+}
