b2393c68cd12dabdfdd2c9ff6b7c68140dc9fe94
AndreyB <metafizick@ya.ru>
2021-03-13 00:52:58 +0300

    Task-4

---
+++ Lesson-4/Task-4/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int number = EntNumb();
+            int fibNumber = Fibonacci(number);
+            Console.WriteLine($"Fibonacci number from {number} = {fibNumber}"); 
+
+        }
+        public static int EntNumb()
+        {
+            bool numBool;
+            int num;
+            do
+            {
+                Console.WriteLine("Please, enter number");
+                numBool = Int32.TryParse(Console.ReadLine(), out num);
+                if (numBool == false)
+                {
+                    Console.WriteLine("Incorrected value of number. Try again");
+                }
+            }
+            while (numBool == false);
+            return num;
+        }
+        public static int Fibonacci(int numb)
+        {
+            if (numb == 0)
+            {
+                return numb;
+            }
+            return Fibonacci(numb - 1) + Fibonacci(numb - 2);
+        }
+    }
+}
