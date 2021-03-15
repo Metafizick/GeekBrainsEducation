68c92bc4a0bd807ebb11fe466f50e3e4108db4bb
AndreyB <metafizick@ya.ru>
2021-03-16 01:28:19 +0300

    Task-1

---
+++ Lesson-5/Task-1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, write something, please");
+            File.WriteAllText("textFile.txt", Console.ReadLine());
+            string text = File.ReadAllText("textFile");
+            Console.WriteLine(text);
+        }
+    }
+}
