4bab24cba3ca95d19b551a295e585fa8af3404f2
AndreyB <metafizick@ya.ru>
2021-03-16 03:19:01 +0300

    Task-4 at works

--- Lesson-5/Task-3/Program.cs
+++ Lesson-5/Task-3/Program.cs
@@ -8,27 +8,29 @@ namespace Task_3
         static void Main(string[] args)
         {
             Console.WriteLine("How much numbers you want to write?");
-            int numbersLenght = number();
+            int numbersLenght = Number(); //Convert.ToInt32(Console.ReadLine());
             byte[] numbers = new byte [numbersLenght];
             for (int i =0; i<numbersLenght; i++)
             {
                 Console.WriteLine($"Enter {i + 1} number");
-                numbers[i] = number();
+                numbers[i] = Number();
             }
             File.WriteAllBytes("numbers.bin", numbers);
 
             
         }
-        static byte number()
+        static byte Number()
         {
             byte num;
+            bool numBool;
             do
             {
-                if (byte.TryParse(Console.ReadLine(), out num) == false | num < 0 | num > 255)
+                numBool = byte.TryParse(Console.ReadLine(), out num);
+                if ( numBool == false | num < 0 | num > 255)
                 {
                     Console.WriteLine("Incorrected value of number (0..255");
                 }
-            } while (byte.TryParse(Console.ReadLine(), out num) == false | num < 0 | num > 255);
+            } while (numBool == false | num < 0 | num > 255);
             return num;
         }
     }
---
+++ Lesson-5/Task-4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace Task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string workDir = @"C:\";
+            string[] entries = Directory.GetFileSystemEntries(workDir, "*", SearchOption.AllDirectories);
+            for (int i = 0; i < entries.Length; i++)
+            {
+                Console.WriteLine(entries[i]);
+            }
+
+        }
+
+    }
+}
