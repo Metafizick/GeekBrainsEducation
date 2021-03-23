8042d6d4e4acc5afad448d8409820c5e46308634
AndreyB <metafizick@ya.ru>
2021-03-23 19:17:46 +0300

    Complete

---
+++ Lesson-8/Lesson -8.1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson__8._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string greeting = Properties.Settings.Default.Greeting;
+            Console.WriteLine(greeting);
+            if (string.IsNullOrEmpty(Properties.Settings.Default.UserName))
+            {
+
+                Console.WriteLine("Enter name of user:");
+                Properties.Settings.Default.UserName = Console.ReadLine();
+                Properties.Settings.Default.Save();
+            }
+            if (string.IsNullOrEmpty(Properties.Settings.Default.Age))
+            {
+                Console.WriteLine("Enter age of user:");
+                Properties.Settings.Default.Age = Console.ReadLine();
+                Properties.Settings.Default.Save();
+            }
+            if (string.IsNullOrEmpty(Properties.Settings.Default.Profession))
+            {
+                Console.WriteLine("Enter profession of user:");
+                Properties.Settings.Default.Profession = Console.ReadLine();
+                Properties.Settings.Default.Save();
+            }
+            string userName = Properties.Settings.Default.UserName;
+            string age = Properties.Settings.Default.Age;
+            string profession = Properties.Settings.Default.Profession;
+            Console.WriteLine($"Good day to you {userName}!\n Your age is {age} and you are {profession}");
+            Console.ReadKey();
+        }
+    }
+}
