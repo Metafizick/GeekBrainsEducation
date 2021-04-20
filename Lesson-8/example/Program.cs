fe2f80b8cde0ff228632a1b4c1e7622b11e537d5
AndreyB <metafizick@ya.ru>
2021-04-21 00:35:40 +0300

    Done

---
+++ Lesson-8/example/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace example
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            Stopwatch s = new Stopwatch();
+            s.Start();
+            int sum = 0;
+            for (int i = 0; i < 2_000_000_000; i++)
+            {
+                sum += i;
+            }
+            s.Stop();
+            Console.WriteLine(s.ElapsedMilliseconds);
+            Console.ReadLine();
+        }
+
+    }
+}
