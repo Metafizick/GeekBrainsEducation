fb6de150f547a47abeece588dde90f3be8ffe738
AndreyB <metafizick@ya.ru>
2021-04-21 18:53:18 +0300

    Done

---
+++ Algorithmus/Lesson-1/Task-2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    class Program
+    {
+        public static int StrangeSum(int[] inputArray)
+        {
+            int sum = 0;
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                for (int j = 0; j < inputArray.Length; j++)
+                {
+                    for (int k = 0; k < inputArray.Length; k++)
+                    {
+                        int y = 0;
+
+                        if (j != 0)
+                        {
+                            y = k / j;
+                        }
+
+                        sum += inputArray[i] + i + k + j + y;
+                    }
+                }
+            }
+
+            return sum;
+        }
+        /* Асимптотическая сложность метода StrangeSum O(2+3n^3), 
+         * где n = inputArray.Length. 2 получили из 2 действий, присвоения и возврата значения
+         * коэффициент 3 - количество действий во внутреннем массиве
+         * n^3 - согласно 4 правилу
+         * Применяя правила несущественности констант, получим итоговую сложность O(n^3)
+         */
+        static void Main(string[] args)
+        {
+            int[] exampleArray = new int[4];
+            int sum = StrangeSum(exampleArray);
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Algorithmus/Lesson-1/Task-3/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int number = EntNumb();
+            int fibNumberRec;
+            int fibNumberFor;
+            if (number >= 0)
+            {
+                fibNumberRec = FibonacciRecPos(number);
+                fibNumberFor = FibonacciForPos(number);
+            }
+            else
+            {
+                fibNumberRec = FibonacciRecNeg(number);
+                fibNumberFor = FibonacciForNeg(number);
+            }
+
+            Console.WriteLine($"Fibonacci number from {number} = {fibNumberRec} with Recursion and {fibNumberFor} with For cycle");
+            Console.ReadKey();
+
+        }
+        public static int EntNumb()
+        {
+            bool numBool;
+            int num;
+            do
+            {
+                Console.WriteLine("Please, enter number");
+                numBool = Int32.TryParse(Console.ReadLine(), out num);
+                if (numBool == false)
+                {
+                    Console.WriteLine("Incorrected value of number. Try again");
+                }
+            }
+            while (numBool == false);
+            return num;
+        }
+        public static int FibonacciRecPos(int numb)
+        {
+            if (numb == 0)
+            {
+                return 0;
+            }
+            else if (numb == 1)
+            {
+                return 1;
+            }
+            return FibonacciRecPos(numb - 2) + FibonacciRecPos(numb - 1);
+        }
+        public static int FibonacciRecNeg(int numb)
+        {
+            if (numb == -1)
+            {
+                return -1;
+            }
+            else if (numb == -2)
+            {
+                return -1;
+            }
+            return FibonacciRecNeg(numb + 2) + FibonacciRecNeg(numb + 1);
+        }
+        public static int FibonacciForPos(int numb)
+        {
+            int a0 = 0;
+            int a1 = 1;
+            int t = a1;
+            for (int i = 1; i<numb; i++ )
+            {
+                t = a0 + a1;
+                a0 = a1;
+                a1 = t;
+            }
+            
+           switch (numb)
+           {
+                case 0:
+                    return 0;
+                case 1:
+                    return 1;
+                default:
+                    return t;              
+           };
+        }
+        public static int FibonacciForNeg(int numb)
+        {
+            int a0 = - 1;
+            int a1 = - 1;
+            int t = a1;
+            for (int i = - 2; i > numb; i--)
+            {
+                t = a0 + a1;
+                a0 = a1;
+                a1 = t;
+            }
+
+            switch (numb)
+            {
+                case -1 :
+                    return -1;
+                case -2 :
+                    return -1;
+                default:
+                    return t;
+            };
+        }
+    }
+}
