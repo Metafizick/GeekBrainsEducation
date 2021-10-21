ab2c235d42246001d1d79ba8758b97f8e1e63990
AndreyB <metafizick@ya.ru>
2021-10-21 20:42:36 +0300

    Lesson-6 start

---
+++ Algorithmus/Lesson-6/Lesson-6/Node.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson_6
+{
+    class Node
+    {
+        public string Value { get; set; }
+        public List<Edge> Edges { get; set; }
+    }
+}
