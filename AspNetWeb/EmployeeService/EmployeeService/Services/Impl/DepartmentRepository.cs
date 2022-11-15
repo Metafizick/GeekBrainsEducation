c518b49162eb4aaaa09eb91893df08049b2e30c8
metafizick <metafizick@ya.ru>
2022-11-15 17:10:06 +0300

    Lesson3

---
+++ AspNetWeb/EmployeeService/EmployeeService/Services/Impl/DepartmentRepository.cs
@@ -0,0 +1,33 @@
+﻿using EmployeeService.Data;
+using EmployeeService.Models;
+
+namespace EmployeeService.Services.Impl
+{
+    public class DepartmentRepository : IDepartmentRepository
+    {
+        public int Create(Department data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Department> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Department GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Department data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
