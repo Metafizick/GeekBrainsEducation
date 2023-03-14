50dd5edfb008cd582853a7dec9c8c3e03a2a5daf
metafizick@ya.ru <metafizick@ya.ru>
2023-03-14 15:41:20 +0300

    Lesson-3, 0.40

---
+++ Secure development/CardStorageService/CardStorageService/Services/Implementations/AuthenticateService.cs
@@ -0,0 +1,55 @@
+﻿using CardStorageService.Data;
+using CardStorageService.Models;
+using CardStorageService.Models.Requests;
+using CardStorageService.Utils;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CardStorageService.Services.Implementations
+{
+    public class AuthenticateService : IAuthenticateService
+    {
+        #region Services
+        private readonly IServiceScopeFactory _serviceScopeFactory;
+        #endregion
+        private readonly Dictionary<string, SessionInfo> _sessions = 
+            new Dictionary<string, SessionInfo>();
+        public AuthenticateService(IServiceScopeFactory serviceScopeFactory)
+        {
+            _serviceScopeFactory = serviceScopeFactory;
+        }
+        public AuthenticationResponse Login(AuthenticationRequest authenticationRequest)
+        {
+            using IServiceScope scope = _serviceScopeFactory.CreateScope();
+            CardStorageServiceDbContext context = scope.ServiceProvider.GetRequiredService<CardStorageServiceDbContext>();
+
+            Account account = 
+                !string.IsNullOrWhiteSpace(authenticationRequest.Login) ? 
+                FindAccountByLogin(context, authenticationRequest.Login) : null;
+            if (account == null)
+            {
+                return new AuthenticationResponse
+                {
+                    Status = AuthenticationStatus.UserNotFound
+                };
+            }
+            if (!PasswordUtils.VerifyPassword(authenticationRequest.Password, account.PasswordSalt, account.PasswordHash)) 
+            {
+                return new AuthenticationResponse 
+                { 
+                    Status = AuthenticationStatus.InvalidPassword 
+                };
+            }
+        }
+        private Account FindAccountByLogin(CardStorageServiceDbContext context, string login)
+        {
+            return context.Accounts.
+                FirstOrDefault(account => account.EMail == login);
+        }
+        public SessionInfo GetSessionInfo(string sessionToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        
+    }
+}
