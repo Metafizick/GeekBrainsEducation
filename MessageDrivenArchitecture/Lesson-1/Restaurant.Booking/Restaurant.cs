c07bb4b3765ccb70bf49e342fabe474dc3316eb9
metafizick@ya.ru <metafizick@ya.ru>
2023-04-04 15:45:08 +0300

    Lesson-1_text_for_methodichka

---
+++ MessageDrivenArchitecture/Lesson-1/Restaurant.Booking/Restaurant.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant.Booking
+{
+    public class Restaurant
+    {
+        private readonly List<Table> _tables = new(); 
+        public Restaurant() 
+        {
+            for (ushort i = 1; i <= 10; i++)
+            {
+                _tables.Add(new Table(i)); 
+            }
+        }
+        public void BookFreeTable(int countOfPerson)
+        {
+            Console.WriteLine("Добрый день! Подождите секунду я подберу столик и подтвержу вашу бронь, оставайтесь на линии");
+            var table = _tables.FirstOrDefault(t => t.SeatsCount > countOfPerson
+                                                        && t.State == State.Free );
+            Thread.Sleep(1000*5);
+            Console.WriteLine(table is null
+                ? $"К сожалению сейчас все столики заняты"
+                : $"Готово! Ваш столик номер {table.Id}");
+        }
+        public void BookFreeTableAsync(int countOfPerson)
+        {
+            Console.WriteLine("Добрый день! Подождите секунду я подберу столик и подтвержу вашу бронь, вам придёт уведомление");
+            Task.Run(async () => 
+            {
+                var table = _tables.FirstOrDefault(t => t.SeatsCount > countOfPerson
+                                                        && t.State == State.Free);
+                await Task.Delay(1000*5);
+                table?.SetState(State.Booked);
+                Console.WriteLine(table is null
+                ? $"К сожалению сейчас все столики заняты"
+                : $"Готово! Ваш столик номер {table.Id}");
+            });
+        }
+    }
+}
---
+++ MessageDrivenArchitecture/Lesson-1/Restaurant.Booking/Table.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant.Booking
+{
+    public class Table
+    {
+        public State State { get; private set; }
+        public int SeatsCount { get; }
+        public int Id { get; }  
+        //private Random Random { get; }
+        public Table(int id) 
+        { 
+            Id = id;
+            State = State.Free;
+            SeatsCount = 2; //Random.Next(2, 5);
+        }
+        public bool SetState(State state)
+        {
+            if (state == State)
+                return false;
+            State = state;
+            return true;
+        }
+    }
+}
